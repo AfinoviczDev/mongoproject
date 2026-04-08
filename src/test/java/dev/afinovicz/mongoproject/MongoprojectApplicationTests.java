2614e801662314117a15512a090559cc29577562
AfinoviczDev <jpafinovicz@gmail.com>
2026-04-08 15:30:24 -0300

    first commit

---
+++ src/test/java/dev/afinovicz/mongoproject/MongoprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.afinovicz.mongoproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MongoprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
